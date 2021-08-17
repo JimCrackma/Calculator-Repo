e8266be34d007c42b0b92e4a8dc0aaa4090d5ce0
JimCrackma <89090580+JimCrackma@users.noreply.github.com>
2021-08-17 18:51:31 +0200

    added calculations
    my first attempt at drawing

--- src/Calculations.java
+++ src/Calculations.java
@@ -1,2 +1,22 @@
 public class Calculations {
+
+    public int addition(int number1, int number2){
+
+        return number1 + number2;
+    }
+
+    public int substraction(int number1, int number2){
+
+        return number1 - number2;
+    }
+
+    public int multiply(int number1, int number2){
+
+        return number1 * number2;
+    }
+
+    public float division(float number1, float number2){
+
+        return number1 / number2;
+    }
 }
--- src/Calculator.java
+++ src/Calculator.java
@@ -2,5 +2,7 @@ public class Calculator {
 
     public static void main(String[] args){
 
+        Calculations Calc = new Calculations();
+        System.out.println(Calc.division(1,2));
     }
 }
